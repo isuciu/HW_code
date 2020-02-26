ab85e786a4453dd287c1f0f50066f1d2620edb03
isuciu <57483865+isuciu@users.noreply.github.com>
2020-02-26 14:59:21 +0100

    Update serial_RPi_commands.ino
    handle more complex commands from RPi

--- A0/serial_RPi_commands.ino
+++ A0/serial_RPi_commands.ino
@@ -1,17 +1,27 @@
 
 const byte interruptPin = 8;
 volatile bool listening_state = 0;
-String inString;
-int x;
-String words[3];
-int i;
-int maxWords = 3;
+String inString, remainingstring;
+String myArray[5];
+String commandWords[2];
 byte reading, real;
 int test_pin = 6; 
 int triggerRPIpin=7;
-int counter = 0;
-String some_answer;
 String SenMLdata;
+int cmdtype,sensortype, num_param;
+
+enum CmdType {
+  Read,
+  Config,
+  Actuate  
+};
+
+
+enum SensorType{
+  analog,
+  digital,
+  spi
+};
 
 void setup() {
 pinMode(test_pin, INPUT);  
@@ -29,7 +39,7 @@ void IncomingCmd(){
   
 }
 
-String getWords(String data, char separator, int index)
+String obtainArray(String data, char separator, int index)
 {
   int found = 0;
   int strIndex[] = {0, -1};
@@ -45,23 +55,28 @@ String getWords(String data, char separator, int index)
 }
 
 
-void Process(String cmd1, String cmd2, String cmd3)
+void ProcessReading(int sensortype, String param_list[5])
 {
+  SenMLdata = "\n";
   Serial.println("Processing command");
-  if (cmd2 == "Analog" || cmd2 == "analog" || cmd2 == "ANALOG")
+  if (sensortype == 0)
   {
      Serial.println("analog read");
-     reading = analogRead(cmd3.toInt());
+     reading = analogRead(param_list[0].toInt());
      Serial.println(reading);
-     SenMLdata= "[{\"bn\":\"ArduinoMKR1000\",\"pinType\":\""+String(cmd2)+"\",\"pinNb\":"+String(cmd3)+",\"pinValue\":"+String(reading)+"}]\n";
+     SenMLdata= "[{\"bn\":\"ArduinoMKR1000\",\"pinType\":\""+String(sensortype)+"\",\"pinNb\":"+String(param_list[0])+",\"pinValue\":"+String(reading)+"}]\n";
   } 
-  else if (cmd2 == "Digital" || cmd2 == "digital" || cmd2 == "DIGITAL")
+  else if (sensortype == 1)
   {
     Serial.println("digital read");
-    reading = digitalRead(cmd3.toInt());
+    reading = digitalRead(param_list[0].toInt());
     Serial.println(reading);
-    SenMLdata= "[{\"bn\":\"ArduinoMKR1000\",\"pinType\":\""+String(cmd2)+"\",\"pinNb\":"+String(cmd3)+",\"pinValue\":"+String(reading)+"}]\n";
+    SenMLdata= "[{\"bn\":\"ArduinoMKR1000\",\"pinType\":\""+String(sensortype)+"\",\"pinNb\":"+String(param_list[0])+",\"pinValue\":"+String(reading)+"}]\n";
   }
+  else
+    Serial.println("SPI PLACEHOLDER");
+
+    
   Serial.println("OUTPUT 1 on pin 7");
   digitalWrite(triggerRPIpin, 1);
   //some_answer = "Read value counter =" + String(counter) + "\n";
@@ -71,6 +86,28 @@ void Process(String cmd1, String cmd2, String cmd3)
   //counter = counter + 1;
 }
 
+void SplitCommand(String command, String fullArray)
+{
+
+  cmdtype = command.charAt(0)- '0'; //cmdtype; extarct ASCII for zero
+  sensortype = command.charAt(1) - '0'; //sensortype
+  num_param = command.charAt(2) - '0'; //param list size
+  
+  remainingstring = fullArray;
+  Serial.println(cmdtype);
+  Serial.println(num_param);
+  for (int i=0; i<num_param; i++)
+  {
+    myArray[i] = obtainArray(fullArray, ',', 0);  
+    remainingstring = fullArray.substring(myArray[i].length()+2, fullArray.length());
+    fullArray = remainingstring;
+    Serial.println(myArray[i]);
+  }
+
+  if (cmdtype == 0) //read
+    ProcessReading(sensortype, myArray); 
+  
+}
   
 void loop() {
 
@@ -82,11 +119,12 @@ void loop() {
         inString = Serial1.readString();
       }
       Serial.println(inString);
-      for (i=0; i<=maxWords-1; i++) {
-          words[i] = getWords(inString, ' ', i);
-          Serial.println(words[i]);
-      }
-      Process(words[0], words[1], words[2]);
+      commandWords[0] = obtainArray(inString, ' ', 0); //get first word containing CmdType, SensorType, Num_param
+      remainingstring = inString.substring(5, inString.length()-1);
+      commandWords[1] = obtainArray(remainingstring, ']', 0); //extract the remaining string containing the list of parameters
+      Serial.println(commandWords[0]);
+      SplitCommand (commandWords[0], commandWords[1]);
+      //Process(words[0], words[1], words[2]);
    }
 
 
