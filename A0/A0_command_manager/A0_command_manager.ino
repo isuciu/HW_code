c38ed7b3b78b7e7673319b5c1acad46a8c246d47
isuciu <isuciu0@uoc.edu>
2020-03-03 13:53:45 +0100

    adding onewire capability. scheduling jobs in a pipeline

--- A0/A0_command_manager/A0_command_manager.ino
+++ A0/A0_command_manager/A0_command_manager.ino
@@ -1,4 +1,6 @@
 
+#include <OneWire.h>
+
 String inString, remainingstring;
 String myArray[5];
 String commandWords[2];
@@ -17,7 +19,8 @@ enum CmdType {
 enum SensorType{
   analog,
   digital,
-  spi
+  spi,
+  onewire
 };
 
 void setup() {
@@ -60,6 +63,12 @@ void ProcessReading(int sensortype, String param_list[5])
     SenMLdata= "[{\"bn\":\"ArduinoMKR1000\",\"pinType\":\""+String(sensortype)+"\",\"pinNb\":"+String(param_list[0])+",\"pinValue\":"+String(reading)+"}]\n";
     Serial.print(SenMLdata); //IMPORTANT! DO NOT PUT PRINTLN, AS THE STRING ALREADY CONTAINS \n
   }
+  else if (sensortype == 3)
+  {
+    float value = OneWireRead(param_list[0].toInt());
+    SenMLdata= "[{\"bn\":\"ArduinoMKR1000\",\"pinType\":\""+String(sensortype)+"\",\"pinNb\":"+String(param_list[0])+",\"pinValue\":"+String(value)+"}]\n";
+    Serial.print(SenMLdata); //IMPORTANT! DO NOT PUT PRINTLN, AS THE STRING ALREADY CONTAINS \n
+  }
   else
     Serial.println("SPI PLACEHOLDER");
 
@@ -85,6 +94,53 @@ void SplitCommand(String command, String fullArray)
 
   if (cmdtype == 0) //read
     ProcessReading(sensortype, myArray); 
+  
+}
+
+
+float OneWireRead(int one_pin){
+  OneWire ds(one_pin); 
+  byte data[12];
+  byte addr[8];
+
+  if ( !ds.search(addr)) {
+      //no more sensors on chain, reset search
+      ds.reset_search();
+      return -1000;
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+      //Serial.println("CRC is not valid!");
+      return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+      //Serial.print("Device is not recognized");
+      return -1000;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+  byte present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read Scratchpad
+
+
+  for (int i = 0; i < 9;  i++  ) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+
+  ds.reset_search();
+
+  byte MSB = data[1];
+  byte LSB = data[0];
+
+  float Read = ((MSB << 8) | LSB); //using two's compliment
+  float Sum = Read / 16;
+
+  return Sum;
   
 }
   
--- RPI/RPi_command_manager.py
+++ RPI/RPi_command_manager.py
@@ -1,6 +1,6 @@
 #this is sending data over ubuntu serial to arduino main serial (not gpio to gpio)
-from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
+import sched
 import serial
 import time
 import json
@@ -17,6 +17,7 @@ class SensorType(Enum):
 	analog = 0
 	digital = 1
 	spi = 2
+	onewire = 3
 
 
 def create_cmd(cmdtype, sensortype, param_list):
@@ -24,23 +25,27 @@ def create_cmd(cmdtype, sensortype, param_list):
 	serialcmd = str(CmdType[cmdtype].value) + str(SensorType[sensortype].value) + str(num_param) + ' ' + str(param_list)
 	return serialcmd
 
-def TransmitThread(ser, serialcmd):
+def TransmitThread(ser, serialcmd, periodicity):
+	global no_answer_pending
+	now = time.time()
+	scheduler.enterabs(now + periodicity, 1, TransmitThread, (ser, serialcmd, periodicity))
+	no_answer_pending = False
 	print (time.time(), ' ', serialcmd)
 	ser.write(serialcmd.encode('utf-8'))
-	time.sleep(3)
-	scheduler.add_job(ReceiveThread, args=[ser, serialcmd1]) #schedules a job to be run immediately
+	scheduler.enterabs(now, 1, ReceiveThread, (ser, serialcmd))
 
 def ReceiveThread(ser, serialcmd):
 		print (time.time(), " The serial cmd transmitted was : ", serialcmd)
 		global no_answer_pending
 		global received_answer
-		if ser.inWaiting()>0:
-			received_answer=True
-			no_answer_pending =True
-			print("manage answer")
-			response = ser.readline()
-			response = response.decode('utf-8')
-			print (str(response))
+		while no_answer_pending == False:
+			if ser.inWaiting()>0:
+				received_answer=True
+				no_answer_pending =True
+				print("manage answer")
+				response = ser.readline()
+				response = response.decode('utf-8')
+				print (str(response))
 	#		if len(response)>2:
 	#			data = json.loads(response) #a SenML list
 	#			for item in data:
@@ -50,7 +55,24 @@ def ReceiveThread(ser, serialcmd):
         #                		print (item["pinValue"])
 		#	else:
 		#		print("no content in the answer") #only the new line character
-			print (time.time(), " End RX processing")
+				print (time.time(), " End RX processing")
+def get_config():
+	delay = {}
+	serialcmd = {}
+	periodicity = {}
+	serialcmd[1] = create_cmd("read", "analog", [5])
+	serialcmd[2]= create_cmd("read", "digital", [3])
+	serialcmd[3] = create_cmd("read", "onewire", [0])
+
+	periodicity[1]=30
+	periodicity[2]=30
+	periodicity[3]=30
+
+	delay[1] = 0
+	delay[2] = delay[1] + 3
+	delay[3] = delay[2] + 3
+
+	return delay, serialcmd, periodicity
 
 
 if __name__ == "__main__":
@@ -58,22 +80,16 @@ if __name__ == "__main__":
 	ser = serial.Serial(
         	port='/dev/ttyACM0',
         	baudrate=9600)
-	serialcmd1 = create_cmd("read", "analog", [5])
-	serialcmd2 = create_cmd("read", "digital", [3])
-	serialcmd3 = create_cmd("read", "spi", [5, 1, 1])
-	scheduler = BackgroundScheduler()
+	delay, serialcmd, periodicity = get_config()
+	size = len(delay)
 
-	#add an offset to each interval?
-	rnd_offset = randint(5, 10) #a random nb of seconds between 5 and 10
-	scheduler.add_job(TransmitThread,'interval', (ser, serialcmd1), seconds=5 + rnd_offset)
-
-	rnd_offset = randint(5, 10)
-	scheduler.add_job(TransmitThread,'interval', (ser, serialcmd2), seconds=5 + rnd_offset)
+	scheduler = sched.scheduler(time.time, time.sleep)
+	now = time.time()
 	
-	rnd_offset = randint(5, 10)
-	scheduler.add_job(TransmitThread,'interval', (ser, serialcmd3), seconds=25 + rnd_offset)
-
-	scheduler.start()
+	for i in range(1, size+1):
+		scheduler.enterabs(now + delay[i], 1, TransmitThread, (ser, serialcmd[i], periodicity[i]))
+	
+	scheduler.run()
 	print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
 	try:# This is here to simulate application activity (which keeps the main thread alive).
